2ec1675303155b7845ebb9c9a99761a8747fc6e4
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-03-08 17:34:47 +0100

    garage

--- garage/db.js
+++ garage/db.js
@@ -9,31 +9,67 @@ mongoose.connect(
   );
 
 const carsSchema = new mongoose.Schema({
-    marque: String,
-    modèle: String,
-    année: Number,
+    brand: String,
+    model: String,
+    year: Number,
+    created: Date,
 });
 
 
 const carModel = mongoose.model(" voitures", carsSchema);
 
-carModel.deleteMany({}).then(() => {
+/*carModel.deleteMany({}).then(() => {
     carModel.create([
         {
-            marque: "Renault",
-            modèle:"Espace",
-            année: 1999,
+            brand: "Renault",
+            model:"Espace",
+            year: 1999,
         },
         {
-            marque: "Renault",
-            modèle:"Scenic",
-            année: 2004,
+            brand: "Renault",
+            model:"Scenic",
+            year: 2004,
         },
         {
-            marque: "Peugeot",
-            modèle:"308",
-            année: 2017,
+            brand: "Peugeot",
+            model:"308",
+            year: 2017,
         },
         
     ])
-})
+});*/
+
+/*carModel.findById({
+   _id: "60463bef9e04b33a00c75e81"
+}).then((response) => console.log(response));*/
+
+
+/*carModel.find({
+   year: {$gt: 2015}
+}).then((response) => console.log(response));*/
+
+carModel.find({
+   model: /o/
+}).then((response) => console.log(response));
+
+
+/*carModel.update({ model: "Espace" }, {
+    year : "2000"
+}).then((response) => console.log(response));*/
+
+/*carModel.deleteMany({
+    brand: "Renault",
+}).then((response) => console.log(response));*/
+
+carModel.insertMany([
+    {
+        brand: "Aston Martin",
+        model:"DB89",
+        year: 2010,
+    },
+    {
+        brand: "Range Rover",
+        model:"Discovery Sport",
+        year: 2017,
+    },
+]).then((response) => console.log(response));
